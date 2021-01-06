f9f4dd4f99318d4b4fb4978ae30144f3fcecb823
Cebion <cebion@Cebion-Portable.localdomain>
2021-01-07 00:39:53 +0100

    Fix Uninstall script

--- solarus/uninstall.sh
+++ solarus/uninstall.sh
@@ -1,14 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 # Copyright (C) 2020-present Fewtarius
 
-INSTALLPATH="/storage/roms/ports"
-PKG_NAME="solarus"
+# Load functions needed to send messages to the console
+. /etc/profile
 
-rm -rf "${INSTALLPATH}/${PKG_NAME}"
-rm -f "${INSTALLPATH}/solarus.sh"
+INSTALL_PATH="/storage/roms/gamedata/solarus"
+IMAGE_PATH="/storage/roms/gamedata/images"
+CFG="/storage/.emulationstation/es_systems.cfg"
+
+if grep -q '<name>solarus</name>' "$CFG"
+then
+	xmlstarlet ed -L -P -d "/systemList/system[name='solarus']" $CFG
+fi
+
+if [ -f /storage/.config/emulationstation/scripts/drastic.sh ]
+then
+  rm -f /storage/.config/emulationstation/scripts/drastic.sh
+fi
+
 
 for image in system-solarus.png  system-solarus-thumb.png
 do
-  rm "${INSTALLPATH}/images/${image}"
+  rm "${IMAGE_PATH}/${image}"
 done
+
+rm -rf ${INSTALL_PATH}
