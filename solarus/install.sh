a4bc9325ddbafc25c6302ea50ed42beadc107955
Cebion <none_ssj9@hotmail.com>
2021-01-07 23:28:21 +0100

    Add a kill key to binary script

--- solarus/install.sh
+++ solarus/install.sh
@@ -33,10 +33,13 @@ rm ${PKG_FILE}
 
 ### Create the start script
 cat <<EOF >${INSTALLPATH}/${PKG_NAME}/"solarus.sh"
+#!/bin/bash
+source /etc/profile
+jslisten set "solarus-run"
 export LD_LIBRARY_PATH=${INSTALLPATH}/${PKG_NAME}/data:/usr/lib
 cd ${INSTALLPATH}/${PKG_NAME}
   ./bin/solarus-run \$1
-fi
+
 ret_error=\$?
 [[ "\$ret_error" != 0 ]] && (echo "Error executing Solarus.  Please check that you copied your Quest File to /storage/roms/solarus" >/tmp/logs/es_log.txt)
 EOF
